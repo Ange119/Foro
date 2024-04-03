58129b69ba328efcf22c2f852223a58fc34105fb
Angel <amoreyra732@gmail.com>
2024-04-03 12:56:52 +0200

    changes:Post del mensaje

--- mensajes/mensaje.php
+++ mensajes/mensaje.php
@@ -1,27 +1,33 @@
 <?php
+
 require_once "../utils/conexion.php";
-if($_SERVER["REQUEST_METHOD"]==='GET'){
-
-}elseif($_SERVER['REQUEST_METHOD']=== 'POST'){
-    usuarioMensaje($idUsuario,$idCanal,$idMensaje);
-}
-function usuarioMensaje($idMensaje,$idUsuario,$idCanal){
-global $conexion;
- 
-if (!empty($_POST['idMensaje']) && !empty($_POST['idUsuario']) && !empty($_POST['idCanal'])) {
+if ($_SERVER["REQUEST_METHOD"] === 'GET') {
     
-    $idCanal = $_POST['idUsuario'];
-    $idMensaje = $_POST['idCanal'];
-$idContenido = $_POST[''];
-    $result = $conexion->query("INSERT INTO mensajes (id_mensaje, id_usuario, id_canal,id_contenido) VALUES ('$idMensaje','$idCanal','$idUsuario')");
+}elseif ($_SERVER["REQUEST_METHOD"] === 'POST') {
 
-    if ($result) {    
-        echo '{"status":"success"}';
+    $idUsuario = $_POST['idUsuario'] ;
+    $idCanal = $_POST['idCanal'] ;
+    $contenido = $_POST['contenido'] ;
+
+    if ($idUsuario !== null && $idCanal !== null && $contenido !== null) {
+        
+        $result = $conexion->query("INSERT INTO mensajes (id_usuario, id_canal, contenido) VALUES ('$idUsuario', '$idCanal', '$contenido')");
+
+        if ($result) {
+            echo '{"status": "succes"}';
+        } else {
+            
+            http_response_code(500);
+            echo '{"error": "Error en la base de datos"}';
+        }
     } else {
-        header("HTTP/1.1 500 Internal Server Error");
-        echo '{"error":"Error en la base de datos"}';
+        http_response_code(400);
+        echo '{"error": "faltan parametros"}';
     }
+} else {
+    http_response_code(405);
+    echo '{"error": "Metodo no permitido "}';
+}
 
-}
-}
 ?>
+
