573a3918ccc346d14c5e740175c07c0ad390a5fe
RicardoSisco <ricardojslgg@gmail.com>
2024-04-08 13:24:09 +0200

    Añadido Rick

--- canales/canales.php
+++ canales/canales.php
@@ -1,56 +1,79 @@
 <?php
 require_once "../utils/conexion.php";
 header('Content-Type: application/json');
-if($_SERVER['REQUEST_METHOD']==='POST'){
-     if(permisoCanal()){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (permisoCanal()) {
         insertarCanal();
-    }else{
-         header("HTTP/1.1 403 Forbidden");
-         echo '{"error":"No tienes permiso para crear canales."}';
+    } else {
+        header("HTTP/1.1 403 Forbidden");
+        echo '{"error":"No tienes permiso para crear canales."}';
     }
- 
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    obtenerCanal($_GET['id']);
 }
- function permisoCanal() {
-     global $conexion;
-    if(!empty($_POST['id_usuario'])) {
-         $id_usuario = $_POST['id_usuario'];
-         // Aquí debes realizar una consulta para obtener el rol y la puntuación del usuario
-        $query = "SELECT puntuacion, rol FROM usuarios WHERE id_usuario = '$id_usuario'";
-         $resultado = $conexion->query($query);
-         if($resultado && $resultado->num_rows > 0) {
-             $usuario = $resultado->fetch_assoc();
-             // Verificar las condiciones para permitir la creación del canal
-             if($usuario['rol'] === 'administrador' || $usuario['puntuacion'] > 100) {
-                 return true;
-             }
-         }
-     }
-     return false;
- }
-function insertarCanal(){
+function permisoCanal()
+{
     global $conexion;
-    if(!empty($_POST['nombre'])&&!empty($_POST['descripcion'])&&!empty($_POST['id_usuario'])){
-        $nombre=$_POST['nombre'];
-        $descripcion=$_POST['descripcion'];
-        $id=$_POST['id_usuario'];
-        $result= $conexion->query("INSERT INTO mensaje (id_usuario) VALUES ('$id')");
-
-        
-        $result = $conexion->query("INSERT INTO usuarios (id_usuario) VALUES ('$id')");
-        $sql="INSERT INTO canales (nombre,descripcion,id_usuario_creador)VALUES ('$nombre','$descripcion',$id);";
-        $result=$conexion->query($sql);
-        
-
-            if ($result) {
-                echo '{"status":"success"}';
-            } else {
-                header("HTTP/1.1 500 Internal Server Error");
-                echo '{"error":"Error con la base de datos."}';
+    if (!empty($_POST['id_usuario'])) {
+        $id_usuario = $_POST['id_usuario'];
+        // Aquí debes realizar una consulta para obtener el rol y la puntuación del usuario
+        $query = "SELECT puntuacion, rol FROM usuarios WHERE id_usuario = '$id_usuario'";
+        $resultado = $conexion->query($query);
+        if ($resultado && $resultado->num_rows > 0) {
+            $usuario = $resultado->fetch_assoc();
+            // Verificar las condiciones para permitir la creación del canal
+            if ($usuario['rol'] === 'administrador' || $usuario['puntuacion'] > 100) {
+                return true;
             }
-        
-    }else {
+        }
+    }
+    return false;
+}
+
+function insertarCanal()
+{
+    global $conexion;
+    if (!empty($_POST['nombre']) && !empty($_POST['descripcion']) && !empty($_POST['id_usuario'])) {
+        $nombre = $_POST['nombre'];
+        $descripcion = $_POST['descripcion'];
+        $id = $_POST['id_usuario'];
+        $result = $conexion->query("INSERT INTO mensaje (id_usuario) VALUES ('$id')");
+
+
+        $result = $conexion->query("INSERT INTO usuarios (id_usuario) VALUES ('$id')");
+        $sql = "INSERT INTO canales (nombre,descripcion,id_usuario_creador)VALUES ('$nombre','$descripcion',$id);";
+        $result = $conexion->query($sql);
+
+
+        if ($result) {
+            echo '{"status":"success"}';
+        } else {
+            header("HTTP/1.1 500 Internal Server Error");
+            echo '{"error":"Error con la base de datos."}';
+        }
+
+    } else {
         header("HTTP/1.1 500 Internal Server Error");
         echo '{"error":"Datos insuficientes para crear un país."}';
+    }
+}
+
+function obtenerCanal($id)
+{
+    global $conexion;
+    if ($id != null) {
+        $info_canal = $conexion->query("SELECT * FROM canales WHERE id_canal = $id");
+        if ($info_canal && $info_canal->num_rows > 0) {
+            echo json_encode($info_canal->fetch_all(MYSQLI_ASSOC));
+            $mensajes_canal = $conexion->query("SELECT * FROM mensajes WHERE id_canal = $id");
+            if ($mensajes_canal->num_rows > 0) {
+                echo json_encode($mensajes_canal->fetch_all(MYSQLI_ASSOC));
+            } else {
+                echo '{"error":"Este canal no tiene mensajes actualmenete."}';
+            }
+        } else
+            echo '{"error":"No se encontraron canales."}';
     }
 }
 
--- mensajes/mensaje.php
+++ mensajes/mensaje.php
@@ -1,51 +1,70 @@
 <?php
 require_once "../utils/conexion.php";
-if($_SERVER["REQUEST_METHOD"]==='GET'){
+if ($_SERVER["REQUEST_METHOD"] === 'GET') {
 
-}elseif($_SERVER['REQUEST_METHOD']=== 'POST'){
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     usuarioMensaje();
+} elseif ($_SERVER["REQUEST_METHOD"] == "PUT") {
+    parse_str(file_get_contents("php://input"), $_PUT);
+    like_mensaje($_PUT["id_mensaje"]);
 }
-function usuarioMensaje(){
-global $conexion;
- 
+function usuarioMensaje()
+{
+    global $conexion;
 
-if (isset($_POST['idUsuario']) && isset($_POST['idCanal'])&&isset($_POST['mensaje'])) {
-    $idUsuario = $_POST['idUsuario'];
-    $idCanal = $_POST['idCanal'];
-    $mensaje = $_POST['mensaje'];
 
-    //consultamos y verificamos el nro de filas del id_usuario
-    $consulta = "SELECT COUNT(*) AS total FROM usuarios WHERE id_usuario = $idUsuario";
-    $resultUsu = $conexion->query($consulta);
+    if (isset($_POST['idUsuario']) && isset($_POST['idCanal']) && isset($_POST['mensaje'])) {
+        $idUsuario = $_POST['idUsuario'];
+        $idCanal = $_POST['idCanal'];
+        $mensaje = $_POST['mensaje'];
+
+        //consultamos y verificamos el nro de filas del id_usuario
+        $consulta = "SELECT COUNT(*) AS total FROM usuarios WHERE id_usuario = $idUsuario";
+        $resultUsu = $conexion->query($consulta);
 
         //consultamos y verificamos el nro de filas del id_Canal
-    $consult = "SELECT COUNT(*) AS total FROM canales WHERE id_canal = $idCanal";
-    $resultCanal = $conexion->query($consult);
+        $consult = "SELECT COUNT(*) AS total FROM canales WHERE id_canal = $idCanal";
+        $resultCanal = $conexion->query($consult);
 
-    $filaUsu = $resultUsu->fetch_assoc();
-    $filaCanal = $resultCanal->fetch_assoc();
+        $filaUsu = $resultUsu->fetch_assoc();
+        $filaCanal = $resultCanal->fetch_assoc();
 
-    // Si el usuario y el canal existen, insertar el mensaje
-    if ($filaUsu['total'] > 0 && $filaCanal['total'] > 0) {
-        // Realizar la consulta SQL para insertar el mensaje
-        $mensaje = "INSERT INTO mensajes (id_usuario, id_canal, contenido) VALUES ('$idUsuario','$idCanal','$mensaje')";
-        $result = $conexion->query($mensaje);
+        // Si el usuario y el canal existen, insertar el mensaje
+        if ($filaUsu['total'] > 0 && $filaCanal['total'] > 0) {
+            // Realizar la consulta SQL para insertar el mensaje
+            $mensaje = "INSERT INTO mensajes (id_usuario, id_canal, contenido) VALUES ('$idUsuario','$idCanal','$mensaje')";
+            $result = $conexion->query($mensaje);
 
-        // Verificar si la inserción fue exitosa
-        if ($result) {    
-            echo '{"status":"success"}';
+            // Verificar si la inserción fue exitosa
+            if ($result) {
+                echo '{"status":"success"}';
+            } else {
+                header("HTTP/1.1 500 Internal Server Error");
+                echo '{"error":"Error en la base de datos"}';
+            }
         } else {
-            header("HTTP/1.1 500 Internal Server Error");
-            echo '{"error":"Error en la base de datos"}';
+            header("HTTP/1.1 400 Bad Request");
+            echo '{"error":"El usuario o el canal no existen"}';
         }
     } else {
         header("HTTP/1.1 400 Bad Request");
-        echo '{"error":"El usuario o el canal no existen"}';
+        echo '{"error":"Faltan parámetros"}';
     }
-} else {
-    header("HTTP/1.1 400 Bad Request");
-    echo '{"error":"Faltan parámetros"}';
-}
 
 }
+
+function like_mensaje($id)
+{
+    global $conexion;
+    $mensaje = $conexion->query("SELECT * FROM mensajes WHERE id_mensaje = $id");
+    if ($fila = $mensaje->fetch_assoc()){
+        $id_usuario = $fila["id_usuario"];
+        $conexion->query("UPDATE mensajes SET likes = likes + 1 WHERE id_mensaje = $id");
+        $conexion->query("UPDATE usuarios SET puntuacion = puntuacion + 1 WHERE id_usuario = $id_usuario");
+        echo $fila["id_usuario"];
+    }
+    else
+        echo $conexion->error;
+}
+
 ?>
